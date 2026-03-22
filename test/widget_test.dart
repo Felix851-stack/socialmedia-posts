b653ad8d1d5f1b354b90a6d5a6712a53f2fa12b7
Felix Tai <141086558+Felix851-stack@users.noreply.github.com>
2026-03-23 00:38:35 +0300

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,7 +6,7 @@ void main() {
   testWidgets('login screen renders the main actions', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
 
-    expect(find.text('Facebook'), findsOneWidget);
+    expect(find.text('Facebook'), findsNWidgets(2));
     expect(find.text('Log in'), findsOneWidget);
     expect(find.text('Create new account'), findsOneWidget);
     expect(find.text('Forgotten password?'), findsOneWidget);
